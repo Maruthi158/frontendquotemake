14da1b4b1b0e368b8a294b61948ea843079d24e4
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2022-02-28 21:48:03 -0600

    uploading the changed code for app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LandingPage from "./screens/LandingPage/LandingPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyQuotes from "./screens/LandingPage/MyQuotes/MyQuotes";
 
 const App = () => (
-  <>
+  <BrowserRouter>
     <Header />
-    <main>
-    <LandingPage />
-    </main>
+    <Routes>
+      <Route path="/" element={<LandingPage />} exact />
+
+      <Route path="/myquotes" element={<MyQuotes />} exact />
+    </Routes>
     <Footer />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
