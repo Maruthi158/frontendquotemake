f2bb2eb8af597686d3e1a665202bcb1404839cd7
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2022-03-04 16:32:59 -0600

    updating src folder with all the required files

---
+++ src/screens/RegisterScreen/RegisterScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MainScreen from "../../components/MainScreen";
+
+const RegisterScreen = () => {
+  return <MainScreen title="Register">Register</MainScreen>;
+};
+
+export default RegisterScreen;
