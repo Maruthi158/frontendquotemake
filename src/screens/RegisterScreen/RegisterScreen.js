f6e54cd4180361b0aff1c0e6c8157e532001331a
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2022-03-22 21:53:13 -0500

    Delete src directory

--- src/screens/RegisterScreen/RegisterScreen.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MainScreen from "../../components/MainScreen";
-
-const RegisterScreen = () => {
-  return <MainScreen title="Register">Register</MainScreen>;
-};
-
-export default RegisterScreen;
